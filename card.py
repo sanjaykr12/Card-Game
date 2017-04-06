48ae85d280a0e5c0a685a4bea557081650aba853
sanjaykr5 <sanjaysnj71@gmail.com>
2017-04-06 21:55:00 +0530

    deck class added

--- card.py
+++ card.py
@@ -21,3 +21,27 @@ class Card:
         return d[value]
     def __str__(self):
          return self.suit + " " + self._convertToFaceValue(self.value)
+class Deck:
+    def __init__(self):
+        self.suits = ["spade","club","diamond","heart"]
+        self.values = range(1,14)
+        self.cards = [];
+        for suit in self.suits:
+            for value in self.values:
+                card = Card(suit,value)
+                self.cards.append(card)
+
+    def draw_random_card(self):
+        card_no = random.randint(1,52)
+        card = self.cards.pop(card_no)
+        return card
+
+    def shuffle_cards(self):
+        print "going to shuffle cards"
+        for i in range (1,52):
+            s = random.randint(1,51)
+            index = 51 % s
+            (self.cards[i],self.cards[index] ) = (self.cards[index],self.cards[i])
+ def display_cards(self,no=52):
+        for card in self.cards[0:no]:
+print card
